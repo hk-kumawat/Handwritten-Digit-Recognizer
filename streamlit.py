611fb70301a83b5b62b02ac1b7f3434264d02e74
hk-kumawat <harshalkumawat100@gmail.com>
2024-11-02 22:03:25 +0530

    save changes

--- streamlit.py
+++ streamlit.py
@@ -10,7 +10,6 @@ try:
     model = tf.keras.models.load_model('mnist_model_enhanced.h5')
 except Exception as e:
     st.error("")
-    model = None  # Ensure model is None if loading fails
 
 # Custom CSS for styling
 st.markdown(
